f9bc419a7d0b682e742a18a7fd9ca81cce0a0863
VishwajeetSawant <vishwajeetnsawant@gmail.com>
2022-04-10 15:30:52 +0530

    comment added

--- lib/doc_ass/authenticate.dart
+++ lib/doc_ass/authenticate.dart
@@ -1,10 +1,10 @@
-
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:remicare/home_pages/login_screen.dart';
 import 'package:remicare/home_pages/doc_ass_home.dart';
+//test
 
 class Authenticate extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
